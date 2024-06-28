36615a5bcf4bc4112f54aa9b7aebcbd357558709
Paulo Victor Lima <pvldotz@gmail.com>
2024-06-27 22:09:44 -0300

    aaaaaa
    aaaaaa

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,29 +1,31 @@
 "use client";
-import React, { ComponentProps, useState } from 'react';
+import React, { ComponentProps, ForwardRefRenderFunction, forwardRef, useState } from 'react';
 
 interface InputProps extends ComponentProps<'input'> {
     text: string
     name: string
 }
 
-export function  Input(pros: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, text, ...rest }, ref) => {
     const [isFocused, setIsFocused] = useState(false);
 
     return (
         <div className="relative input-container">
             <input
-                type="text"
-                name={pros.name}
-                placeholder={isFocused ? '' : pros.text}
+                name={name}
+                placeholder={isFocused ? '' : text}
                 className="w-[372px] h-[68px] bg-gray-300 text-base text-[#1c1c1c] border-[3px] border-gray-300 rounded-md pl-3 pr-3 focus:outline-none focus:border-black focus:bg-transparent focus:text-xl placeholder-black font-bold placeholder-opacity-75"
+                ref={ref}
                 onFocus={() => setIsFocused(true)}
                 onBlur={() => setIsFocused(false)}
             />
             <span
                 className={`absolute bg-white p-2 rounded-full top-0 left-3 transform translate-y-[-50%] text-black font-bold transition-all duration-300 pointer-events-none ${isFocused ? 'top-0 opacity-100' : 'top-1/2 opacity-0'}`}
             >
-                {pros.text}
+                {text}
             </span>
         </div>
     )
 }
+
+export const Input = forwardRef(InputBase);
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,8 +4,15 @@ import { Input } from "../components/Input";
 import { loginUser } from "./actions";
 import { useState } from "react";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
+ const { register, handleSubmit } = useForm()
+
+  function handleSignin(values) {
+    console.log(values)
+  }
+
   return (
     <div className="flex justify-center items-center w-screen h-screen bg-[#1c1c1c] font-sans">
       <div className="flex flex-col justify-center items-center gap-12 w-[500px] h-[700px] rounded-[50px] bg-white">
@@ -15,9 +22,9 @@ export default function Login() {
 
         <h1 className="text-4xl text-black">Faça login</h1>
 
-        <form className="flex flex-col justify-center items-center gap-5">
-          <Input text="NOME DE USUÁRIO" name="login" />
-          <Input text="SENHA" name="password" />
+        <form onSubmit={handleSubmit(handleSignin)} className="flex flex-col justify-center items-center gap-5">
+          <Input text="NOME DE USUÁRIO" type="text" {...register('login')} />
+          <Input text="SENHA" type="password" {...register('password')} />
 
           <button type="submit" className="flex justify-center items-center w-[150px] h-[100px] rounded-[25px] bg-[#B055B2]">
             <ArrowRight color="#1c1c1c" size={'50px'} />
