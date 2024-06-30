007f07ac0a7f958efa0c180c89451bdf3f0c6b52
Paulo Victor Lima <pvldotz@gmail.com>
2024-06-30 16:18:45 -0300

    a
    a

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,17 +2,21 @@
 import { useEffect, useState } from "react";
 
 export default function Profile() {
-    const [cookie, setCookie] = useState<string | null>(null);
 
-    useEffect(() => {
-      const accessToken = localStorage.getItem('accessToken');
-      setCookie(accessToken);
-    }, []);
+  const cookies = document.cookie.split('; ');
+  for (const cookie of cookies) {
+    const [name, value] = cookie.split('=');
+    if (name === 'sessionId') {
+      console.log('Valor do cookie:', value);
+      break;
+    }
+  }
+
   
     return (
       <div>
         <h1>Valor do token:</h1>
-        <p>{cookie}</p>
+
       </div>
     );
 }
