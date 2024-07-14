c296dd434d0188ab1b85b0ab1a6991f57b3c0f5a
Paulo Victor Lima <pvldotz@gmail.com>
2024-07-13 22:36:53 -0300

    patch: adicioanr mudanças no login form

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -8,16 +8,16 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
     ({ placeholder, name, error = null, type, ...rest }, ref) => {
-    const [isFocused, setIsFocused] = useState(false);
+        const [isFocused, setIsFocused] = useState(false);
 
         return (
             <div className="relative input-container">
                 <input
-                    {...rest}
-                    placeholder={isFocused ? '' : placeholder}
-                    className={`w-[372px] h-[68px] bg-gray-300 text-base text-[#1c1c1c] border-[3px] rounded-md pl-3 pr-3 focus:outline-none focus:text-xl placeholder-black font-bold placeholder-opacity-75 ${
+                    className={`w-[372px] h-[68px] bg-gray-300 text-xl text-[#1c1c1c] border-[3px] rounded-md pl-3 pr-3 focus:outline-none placeholder-black font-bold placeholder-opacity-75 ${
                         error ? 'border-red-500 bg-transparent' : 'focus:border-black focus:bg-transparent border-gray-300'
                     }`}
+                    {...rest}
+                    placeholder={isFocused ? '' : placeholder}
                     onFocus={() => setIsFocused(true)}
                     onBlur={() => setIsFocused(false)}
                     type={type}
@@ -25,8 +25,8 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
                     ref={ref}
                 />
                 <span
-                    className={`absolute bg-white p-2 rounded-full top-0 left-3 transform translate-y-[-50%] text-black font-bold transition-all duration-300 pointer-events-none ${
-                    isFocused ? 'top-0 text-base opacity-100' : 'top-1/2 opacity-0'} ${error ? 'text-red-500' : ''}`}
+                    className={`absolute bg-white p-2 rounded-full top-0 left-3 transform translate-y-[-50%] text-black font-bold transition-all duration-300 pointer-events-none
+                        ${isFocused ? 'top-0 text-base opacity-100' : 'top-1/2 opacity-0'} ${error ? 'text-red-500' : ''}`}
                 >
                     {placeholder}
                 </span>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,14 +3,13 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Input";
 import { ArrowRight } from "lucide-react";
 import { loginUser } from "./actions";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Image from "next/image";
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 
 type LoginResponse = {
   message: string;
-/*   user: User | null; */
   token: string;
 };
 
@@ -32,13 +31,13 @@ export default function Login() {
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     try {
-      const formData = new FormData();
-      formData.append('login', values.login);
-      formData.append('password', values.password);
+      const formData = new FormData()
+      formData.append('login', values.login)
+      formData.append('password', values.password)
 
       const response = await loginUser(formData)
-      console.log('Token JWT obtido:', response);
-      localStorage.setItem('accessToken', response.token);
+      console.log('Token JWT obtido:', response)
+      localStorage.setItem('accessToken', response.token)
     } catch (error) {
       console.error('Error logging in:', error)
     }
@@ -46,26 +45,24 @@ export default function Login() {
 
   return (
     <div className="flex justify-center items-center w-screen h-screen bg-[#1c1c1c] font-sans">
-      <div className="flex flex-col justify-center items-center gap-12 w-[500px] h-[700px] rounded-[50px] bg-white">
-        <div className="flex justify-center items-center w-[145px] h-[145px] rounded-[100px] bg-[#1c1c1c]">
-          <Image width={100} height={100} src="./logo_lizard.svg" alt="Logo do projeto" title="Logo de um calango roxo" />
-        </div>
+      <div className="flex flex-col justify-center items-center w-[500px] h-[700px] rounded-[50px] bg-white">
+        <Image width={150} height={150} src="./logo-svg-pvldo.svg" alt="Logo do projeto" title="Logo de um calango roxo" />
 
         <h1 className="text-4xl text-black">Faça login</h1>
 
-        <form onSubmit={handleSubmit(handleSignIn)} className="flex flex-col justify-center items-center gap-5">
+        <form onSubmit={handleSubmit(handleSignIn)} className="flex flex-col m-7 justify-center items-center gap-5">
           <Input 
             type="text" 
             placeholder="Login" 
             error={errors.login}
-            {...register('login')} 
+            {...register('login')}
           />
 
           <Input 
             type="text" 
             placeholder="Senha" 
             error={errors.password}
-            {...register('password')} 
+            {...register('password')}
           />
 
           <button type="submit" className="flex justify-center items-center w-[150px] h-[100px] rounded-[25px] bg-[#B055B2]">
